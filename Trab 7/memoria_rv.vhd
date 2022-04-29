c9e05bb5918a3be4050ef62fa0c0d1ab5d473434
João Pedro de Oliveira Silva <jo88791@gmail.com>
2022-04-29 16:40:33 -0300

    hmmmm

--- Trab 7/memoria_rv.vhd
+++ Trab 7/memoria_rv.vhd
@@ -10,3 +10,44 @@
 --#*                                              #
 --#################################################
 
+library IEEE;
+use IEEE.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+-- Fornecido pelo professor
+entity rom_rv is 
+ port (
+	 address : in std_logic_vector(11 downto 0);
+	 dataout : out std_logic_vector(31 downto 0)
+ );
+end entity rom_rv;
+
+entity ram_rv is
+ port (
+	 clock : in std_logic;
+	 we : in std_logic;
+	 address : in std_logic_vector(11 downto 0);
+	 datain : in std_logic_vector(31 downto 0);
+	 dataout : out std_logic_vector(31 downto 0)
+ );
+end entity ram_rv;
+
+
+architecture RTL of ram_rv is
+	Type ram_type is array (0 to (2**address'length)-1) of std_logic_vector(datain'range);
+	signal ram : ram_type;
+	signal read_address : std_logic_vector(address'range);
+
+    begin
+        process(clock)
+        begin
+            if rising_edge(clock) then
+                if we = '1' then
+                    ram(to_integer(unsigned(address))) <= datain;
+                end if;
+                read_address <= address;
+            end if;       
+        end process;
+        dataout <= ram(to_integer(unsigned(read_address)));
+
+end RTL;
